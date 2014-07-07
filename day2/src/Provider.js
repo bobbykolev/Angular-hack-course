c52464080d75bcc1f5fe83f77b41d93f67bcf5d9
Bobby Kolev <bobbykolev@abv.bg>
2014-07-07 19:34:17 +0300

    scope

--- day2/src/Provider.js
+++ day2/src/Provider.js
@@ -1,68 +1,64 @@
 var Provider = {
-	DIRECTIVES_SUFFIX: 'Directive',
-	CONTROLLERS_SUFFIX: 'Controller',
+    DIRECTIVES_SUFFIX: 'Directive',
+    CONTROLLERS_SUFFIX: 'Controller',
 
-	_providers: {},
-	_cache: {
-		//rootScope: new Scope()
-	},
-	get: function(name, locals){
-		if(typeof this._cache[name] != 'undefined') {
-			console.log('1',this._cache[name])
-			return this._cache[name];
-		} else {
-			this._cache[name] = this.invoke(this._providers[name], locals);
-			console.log('2',this._cache[name]);
-			return this._cache[name];
-		}		
-	},
-	annotate: function(fn){
-		console.log(fn);
-		var FN_ARGS = /\((.*?)\)/,
-		match = fn.toString().match(FN_ARGS),
-		args;
-		if (match && match[1]) {
-			args = match[1].split(',').map(function (arg) {
-				return arg.trim();
-			});
-		}
-		return args || [];
-	},
-	invoke: function(fn, locals){
-		console.log('invoke',fn);
-		locals = locals || {};
-		var args = this.annotate(fn),
-		arr = [];
-		for (var i = 0; i < args.length; i++) {
-			var temp = this.get(args[i], locals);
-			arr.push( locals[args[i]] || temp);
-		}
-		return fn.apply(null, arr);
-	},
-	_register: function (name, fn){
-		console.log('reg',fn);
-		this._providers[name] = fn;
-	},
-	directive: function (name, fn){
-		this._register(name + this.DIRECTIVES_SUFFIX, fn);
-	},
-	controller: function (name, fn){
-		this._register(name + this.CONTROLLERS_SUFFIX, fn);
-	},
-	service: function (name, fn){
-		this._register(name, fn);
-	}
+    _providers: {},
+    _cache: {
+        //rootScope: new Scope()
+    },
+    get: function(name, locals) {
+        if (typeof this._cache[name] != 'undefined') {
+            return this._cache[name];
+        } else {
+            this._cache[name] = this.invoke(this._providers[name], locals);
+            return this._cache[name];
+        }
+    },
+    annotate: function(fn) {
+        var FN_ARGS = /\((.*?)\)/,
+            match = fn.toString().match(FN_ARGS),
+            args;
+        if (match && match[1]) {
+            args = match[1].split(',').map(function(arg) {
+                return arg.trim();
+            });
+        }
+        return args || [];
+    },
+    invoke: function(fn, locals) {
+        locals = locals || {};
+        var args = this.annotate(fn),
+            arr = [];
+        for (var i = 0; i < args.length; i++) {
+            var temp = this.get(args[i], locals);
+            arr.push(locals[args[i]] || temp);
+        }
+        return fn.apply(null, arr);
+    },
+    _register: function(name, fn) {
+        this._providers[name] = fn;
+    },
+    directive: function(name, fn) {
+        this._register(name + this.DIRECTIVES_SUFFIX, fn);
+    },
+    controller: function(name, fn) {
+        this._register(name + this.CONTROLLERS_SUFFIX, fn);
+    },
+    service: function(name, fn) {
+        this._register(name, fn);
+    }
 };
 
 
-Provider.service('Bar', function Bar(){
-	return {
-		getValue: function () {
-			return 42
-		}
-	}
-});
+Provider.service('Bar', function Bar() {
+        return {
+            getValue: function() {
+                return 42;
+            }
+        });
 
-Provider.service('Foo', function Foo(Bar){console.log(Bar.getValue());});
+    Provider.service('Foo', function Foo(Bar) {
+        console.log(Bar.getValue());
+    });
 
-Provider.get('Foo');
+    Provider.get('Foo');
--- day2/src/Scope.js
+++ day2/src/Scope.js
@@ -1,30 +1,41 @@
 function Scope(parent, id) {
-	this.$$watchers = [];
-	this.$$children = [];
-	this.$parent = parent;
-	this.$id = id || 0;
+    this.$$watchers = [];
+    this.$$children = [];
+    this.$parent = parent;
+    this.$id = id || 0;
 
-	this.counter = 0;
-
-	this.$eval = function(data) {
-		switch(typeof data) {
-			case 'function': 
-				fn();
-				break;
-
-			default: 
-			return data;
-				break;
-		}
-	}
+    this.$eval = function(data) {
+        return eval(data.toString());
+    }
 }
 
 Scope.prototype.$watch = function(exp, fn) {
-	var obj = {};
+    var obj = {};
 
-	obj.exp = exp;
-	obj.exp = fn;
-	obj.last = Utils.clone(exp);
+    obj.exp = exp;
+    obj.exp = fn;
+    obj.last = Utils.clone(exp);
 
-	this.watchers.push(obj);
-} 
+    this.$$watchers.push(obj);
+};
+
+Scope.prototype.$new = function() {
+
+    var scope = Object.create(this);
+    Scope.call(scope, this, ++Scope.counter);
+
+    this.$$children.push(this);
+
+    return this;
+};
+
+Scope.counter = 0;
+var a = new Scope();
+
+a.$new();
+console.log(a.$id);
+
+
+/*function evall(data) {
+    return eval(data.toString());
+}*/
